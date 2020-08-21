ff91a674a7f6fe3a57c69e8de891e0d81ebfbd1b
kang <licheeking@gmail.com>
2020-08-21 23:08:57 +0800

    third commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,5 @@
 /**
- * Created by junxie on 18/5/27.
+ * Created by kang on 18/5/27.
  */
-import React from 'react';
-import ReactDom from 'react-dom';
+import Toolbar from './component/TooltipButton/ToolBar';
 import TooltipButton from './component/TooltipButton/TooltipButton';
-import './default.less';
-import './markdown-style.less';
-import './style.less';
-ReactDom.render(<TooltipButton />,
-    document.getElementById('app')
-);
